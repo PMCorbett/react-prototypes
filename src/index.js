30bc0ce5f8e83ea3bc25c16b2550c0ecdc11070e
Patrick Corbett <patrick@crowdlab.com>
2016-02-15 12:08:52 +0000

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './style.css';
+import Component from './components/component';
+
+ReactDOM.render(<Component name='Test' />, document.querySelector('#app'));
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+var path = require('path');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const PATHS = {
+  src: path.join(__dirname, 'src'),
+  build: path.join(__dirname, 'build')
+};
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: PATHS.build,
+    publicPath: '/assets/',
+    filename: 'bundle.js'
+  },
+  devtool: 'source-map',
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract('style-loader', 'css-loader'),
+        include: PATHS.src
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        include: PATHS.src
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin('styles.css')
+  ]
+};
